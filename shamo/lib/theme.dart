f7406f62dbf9f6cce5bf583fce081f568a3b1ef4
Koredeee <m.fatihdaffa@gmail.com>
2023-09-15 02:04:32 +0700

    Added Sign In Page

--- shamo/lib/theme.dart
+++ shamo/lib/theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
+double defaultMargin = 30.0;
 
 const Color primaryColor = Color(0xff6C5ECF);
 const Color secondaryColor = Color(0xff38ABBE);
@@ -12,10 +12,13 @@ const Color backgroundColor2 = Color(0xff2B2937);
 const Color backgroundColor3 = Color(0xff242231);
 const Color primaryTextColor = Color(0xffF1F0F2);
 const Color secondaryTextColor = Color(0xff999999);
+Color subtitleColor = Color(0xff504F5E);
 
-TextStyle primaryStyle = GoogleFonts.poppins(color: primaryTextColor);
-TextStyle secondaryStyle = GoogleFonts.poppins(color: secondaryTextColor);
+TextStyle primaryTextStyle = GoogleFonts.poppins(color: primaryTextColor);
+TextStyle secondaryTextStyle = GoogleFonts.poppins(color: secondaryTextColor);
+TextStyle subtitleTextStyle = GoogleFonts.poppins(color: subtitleColor);
 TextStyle priceTextStyle = GoogleFonts.poppins(color: priceColor);
+TextStyle purpleTextStyle = GoogleFonts.poppins(color: primaryColor);
 
 FontWeight light = FontWeight.w300;
 FontWeight reguler = FontWeight.w400;
