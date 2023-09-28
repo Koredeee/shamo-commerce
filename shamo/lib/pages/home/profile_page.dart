2c5d5b734920d133bdc98af3ac9f449c6d815499
Koredeee <m.fatihdaffa@gmail.com>
2023-09-28 09:11:10 +0700

    Added Profile Page

--- shamo/lib/pages/home/profile_page.dart
+++ shamo/lib/pages/home/profile_page.dart
@@ -6,10 +6,106 @@ import 'package:shamo/theme.dart';
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
+  Widget header() {
+    return AppBar(
+      backgroundColor: backgroundColor1,
+      automaticallyImplyLeading: false,
+      elevation: 0,
+      // agar tidak tembus ke status bar
+      flexibleSpace: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(defaultMargin),
+          child: Row(
+            children: [
+              ClipOval(
+                child: Image.asset('assets/image_profile.png', width: 64),
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hallo, Asep',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 24, fontWeight: semiBold),
+                    ),
+                    Text(
+                      '@asersurasep',
+                      style: subtitleTextStyle.copyWith(fontSize: 16),
+                    )
+                  ],
+                ),
+              ),
+              Image.asset('assets/button_exit.png', width: 20)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget menuItem(String text) {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            text,
+            style: secondaryTextStyle.copyWith(fontSize: 13),
+          ),
+          Icon(
+            Icons.chevron_right,
+            color: primaryTextColor,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget content() {
+    return Expanded(
+        child: Container(
+      padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+      // agar keliatan batasan size content nya
+      width: double.infinity,
+      decoration: BoxDecoration(color: backgroundColor3),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 20),
+          Text(
+            'Account',
+            style:
+                primaryTextStyle.copyWith(fontSize: 16, fontWeight: semiBold),
+          ),
+          menuItem('Edit Profile'),
+          menuItem('Your Orders'),
+          menuItem('Help'),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            'General',
+            style:
+                primaryTextStyle.copyWith(fontSize: 16, fontWeight: semiBold),
+          ),
+          menuItem('Privacy & Policy'),
+          menuItem('Term of Service'),
+          menuItem('Rate App'),
+        ],
+      ),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Page', style: primaryTextStyle),
+    return Column(
+      children: [
+        header(),
+        content(),
+      ],
     );
   }
 }
