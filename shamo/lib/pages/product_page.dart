ddb20ec0b171394baad424d0dad1b5bc139bd4b1
Koredeee <m.fatihdaffa@gmail.com>
2023-09-28 23:37:26 +0700

    Added Product Page SnackBar and Modal

--- shamo/lib/pages/product_page.dart
+++ shamo/lib/pages/product_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace
 
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +19,7 @@ class ProductPage extends StatefulWidget {
 
 class _ProductPageState extends State<ProductPage> {
   int currentIndex = 0;
+  bool isWishList = false;
 
   List familiarShoes = [
     'assets/image_shoes.png',
@@ -31,6 +32,60 @@ class _ProductPageState extends State<ProductPage> {
     'assets/image_shoes.png',
     'assets/image_shoes.png',
   ];
+
+  Future<void> showSuccessDialog() async {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) => Container(
+        width: MediaQuery.of(context).size.width - (2 * defaultMargin),
+        child: AlertDialog(
+          backgroundColor: backgroundColor3,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(Icons.close, color: primaryTextColor))),
+                Image.asset('assets/icon_success.png', width: 100),
+                SizedBox(height: 12),
+                Text(
+                  'Horrayy!',
+                  style: primaryTextStyle.copyWith(
+                      fontSize: 18, fontWeight: semiBold),
+                ),
+                SizedBox(height: 12),
+                Text('Item added successfully!', style: secondaryTextStyle),
+                SizedBox(height: 20),
+                Container(
+                  width: 154,
+                  height: 44,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: primaryColor,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                    ),
+                    child: Text(
+                      'View My Cart',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 16, fontWeight: medium),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   Widget indicator(int index) {
     return Container(
@@ -153,7 +208,42 @@ class _ProductPageState extends State<ProductPage> {
                     ],
                   ),
                 ),
-                Image.asset('assets/button_wishlist.png', width: 46),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      // apabila diklik maka isWishList = true
+                      // lalu diklik maka isWishList = false
+                      isWishList = !isWishList;
+                    });
+
+                    if (isWishList) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          backgroundColor: secondaryColor,
+                          content: Text(
+                            'Has been added to the Wishlist',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          backgroundColor: alertColor,
+                          content: Text(
+                            'Has been removed from the Wishlist',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  child: Image.asset(
+                      isWishList
+                          ? 'assets/button_wishlist_blue.png'
+                          : 'assets/button_wishlist.png',
+                      width: 46),
+                ),
               ],
             ),
           ),
@@ -254,12 +344,17 @@ class _ProductPageState extends State<ProductPage> {
                 bottom: defaultMargin),
             child: Row(
               children: [
-                Container(
-                  width: 54,
-                  height: 54,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/button_chat.png'),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/detail-chat');
+                  },
+                  child: Container(
+                    width: 54,
+                    height: 54,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/button_chat.png'),
+                      ),
                     ),
                   ),
                 ),
@@ -268,7 +363,9 @@ class _ProductPageState extends State<ProductPage> {
                   child: Container(
                     height: 54,
                     child: TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        showSuccessDialog();
+                      },
                       style: TextButton.styleFrom(
                         foregroundColor: primaryColor,
                         shape: RoundedRectangleBorder(
